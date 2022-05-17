941f5e57040736f62a2572429b4fcce159727cda
Padmakshi <padmakshi.ashok@gmail.com>
2022-05-17 10:08:10 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <Navbar.Brand className="font-weight-bold text-muted">
+          <Nav.Link href="/">Opinion Board</Nav.Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
